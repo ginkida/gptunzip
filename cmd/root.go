11050c3a69eb2e50240ab426d211d5fd9a24ef4e
ginkida <Ginkida@gmail.com>
2024-10-10 14:52:22 +0500

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -11,6 +11,7 @@ import (
 
 var repoPath string
 var isParts bool
+var partSize int 
 
 var rootCmd = &cobra.Command{
 	Use:   "gptunzip /path/to/git/repository",
@@ -24,44 +25,51 @@ var rootCmd = &cobra.Command{
 			os.Exit(1)
 		}
 		defer os.RemoveAll(tempDir)
+
 		if err := prompt.UnzipFile(repoPath, tempDir); err != nil {
 			fmt.Printf("Error unzipping file: %v\n", err)
 			os.Exit(1)
 		}
+
 		subdirPath, err := prompt.FindSingleSubdir(tempDir)
 		if err != nil {
 			fmt.Printf("Error finding subdir: %v\n", err)
 			os.Exit(1)
 		}
+
 		fmt.Println("Unzipped to:", subdirPath)
 		repo, err := prompt.ProcessGitRepo(subdirPath)
 		if err != nil {
 			fmt.Printf("Error processing git repo: %v\n", err)
 			os.Exit(1)
 		}
+
 		output, err := prompt.OutputGitRepo(repo)
 		if err != nil {
 			fmt.Printf("Error generating output: %v\n", err)
 			os.Exit(1)
 		}
+
 		saveFunc := prompt.SaveTextAsOneFile
 		if isParts {
 			saveFunc = func(path, text string) error {
-		        	return prompt.SaveTextAsParts(path, text, partSize)
-		    	}
+				return prompt.SaveTextAsParts(path, text, partSize) 
+			}
 		}
+
 		if err := saveFunc(subdirPath, output); err != nil {
 			fmt.Printf("Error saving output: %v\n", err)
 		} else {
 			fmt.Println("The file has been saved in the current directory.")
 		}
+
 		fmt.Printf("Estimated number of tokens: %d\n", prompt.EstimateTokens(output))
 	},
 }
 
 func init() {
 	rootCmd.Flags().BoolVarP(&isParts, "parts", "p", false, "create small parts for chatGPT")
-	rootCmd.Flags().IntVarP(&partSize, "part-size", "s", 3, "size of each part in MB (only used if --parts is enabled)")
+	rootCmd.Flags().IntVarP(&partSize, "part-size", "s", 1, "size of each part in MB (only used if --parts is enabled)")
 }
 
 func Execute() {
